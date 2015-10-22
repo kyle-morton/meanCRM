470c9e9574406feaa4015efc9306ff24b0a24368
kylemorton5770 <kylemortondevelopment@gmail.com>
2015-10-21 22:15:22 -0500

    sending file contents to backend

--- public/app/controllers/fileCtrl.js
+++ public/app/controllers/fileCtrl.js
@@ -10,8 +10,37 @@ angular.module ('fileCtrl', ['postService', 'fileService', 'angularFileUpload'])
 	//used to handle files added to upload section
 	$scope.uploadFile = function(event){
         var fileToUpload = event.target.files[0];
-        console.log('FileCtrl file was selected: ' + fileToUpload.name + " " + fileToUpload.size 
-				+ " " + fileToUpload.getAsDataUrl());
+        console.log('FileCtrl file was selected: ' + fileToUpload.name + " " + fileToUpload.size + " " + fileToUpload.data);
+   		vm.FileUpload(fileToUpload);
+		
+		   
+		   
+		// File.upload(fileToUpload)
+		// 	.then(function(data) {
+		// 		console.log(JSON.stringify(data));
+		// 	});	
+   
     };
+	
+	vm.FileUpload= function(initFile, File) {
+		var reader = new FileReader();  
+		
+		//create onloadend subscriber to handle finished file
+		reader.onloadend = function(evt) {
+			var readFile = reader.result;
+			vm.upload(readFile);
+		};
+		
+		//read file
+		reader.readAsBinaryString(initFile);
+	}
+	
+	vm.upload = function(readFile) {
+		console.log("in upload!");
+		File.upload(readFile) 
+			.then(function(data){
+				console.log(JSON.stringify(data));
+			});
+	}
 	
 });
